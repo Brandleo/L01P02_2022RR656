26b33f75dd6659b868b04877e699acc71d6ae8c4
Brandleo <brandon.repreza@catolica.edu.sv>
2025-03-25 17:16:57 -0600

    modelos

---
+++ L01P02_2022RR656/Models/facultades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace L01P02_2022RR656.Models
+{
+    public class facultades
+    {
+        [Key]
+
+        public int id { get; set; }
+
+        public string ? facultad  { get; set; }
+
+
+
+    }
+}
