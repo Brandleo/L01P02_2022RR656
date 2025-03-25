33f541f9ed822215a05c962d690267a144ef9b93
Brandleo <brandon.repreza@catolica.edu.sv>
2025-03-25 17:28:12 -0600

    controladores

--- L01P02_2022RR656/Models/db_notasDbContext.cs
+++ L01P02_2022RR656/Models/db_notasDbContext.cs
@@ -9,6 +9,12 @@ namespace L01P02_2022RR656.Models
         
         }
 
+        public DbSet <alumnos> alumnos { get; set; }
+        public DbSet <materias> materias { get; set; }
+
+        public DbSet <departamentos> departamentos {  get; set; }
+        
+        public DbSet<facultades> facultades { get; set; }
 
 
     }
