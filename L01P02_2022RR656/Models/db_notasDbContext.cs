9b5b748f8617864fa39311787cddcee3d096f4a9
Brandleo <brandon.repreza@catolica.edu.sv>
2025-03-25 17:02:12 -0600

    Agregar archivos de proyecto.

---
+++ L01P02_2022RR656/Models/db_notasDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2022RR656.Models
+{
+    public class db_notasDbContext : DbContext
+    {
+        public db_notasDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+
+
+    }
+}
